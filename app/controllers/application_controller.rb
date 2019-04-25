144c4497078901f3833f25fcda2e9956eaa46a63
Marior9 <mariorodriguezan@gmail.com>
2019-04-25 00:30:25 -0400

    inprogress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,35 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  get '/articles/new' do
+    erb :new
+  end
+  post '/articles/new' do
+    @article = Article.create(title: params[:title], content: params[:content])
+
+  end
+
+  get '/articles/new' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+  @article = params[:id]
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+      erb :edit
+  end
+
+  delete '/articles/:id' do
+
+
+  end
+
+
+
+
+
 end
